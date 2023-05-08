afbb805dbe11adbb105d5b596855d44bed247912
bswlkh0310 <hhhello0507@dgsw.hs.kr>
2023-05-08 15:38:28 +0900

    5/8 commit
    refactoring

--- app/src/main/java/com/example/diaryapp/MainActivity.kt
+++ app/src/main/java/com/example/diaryapp/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.diaryapp
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.CalendarView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.diaryapp.databinding.ActivityMainBinding
 import com.sothree.slidinguppanel.SlidingUpPanelLayout
 import java.io.*
 import java.time.LocalDate
@@ -14,100 +13,90 @@ import java.time.format.DateTimeFormatter
 
 
 class MainActivity : AppCompatActivity(), DialogInterface {
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     var year: Int? = null
     var month: Int? = null
     var day: Int? = null
-    var shows: SaveDialog? = null
-    private val TAG: String = "로그"
+    private val shows: SaveDialog by lazy { SaveDialog(this, this) }
+    private val tag: String = "로그"
 
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        initCalendar()
+        initCommit()
+    }
 
-        // 캘린더, 날짜 텍스트
-        val calendar: CalendarView = findViewById(R.id.celendar)
-        val date: TextView = findViewById(R.id.date)
-        val drawer: SlidingUpPanelLayout = findViewById(R.id.drawer)
+    private fun initCalendar() {
+        // 오늘 날짜 구하기
+        val today: String = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"))
+        year = today.slice(0..3).toInt()
+        month = today.slice(4..5).toInt()
+        day = today.slice(6..7).toInt()
+        val todayText = "$year / $month / $day"
+        binding.date.text = todayText
+        Log.d(tag, "$today - onCreate() called")
 
-        today() // 오늘 날짜로 초기화
-        drawer.addPanelSlideListener (PanelEventListener()) // 패널 이벤트 => 날짜에 맞는 내용 불러오기
+        // drawer listener 설정
+        binding.drawer.addPanelSlideListener(PanelEventListener())
 
-        // 캘린더에서 날짜 선택 => 날짜 텍스트 변경
-        calendar.setOnDateChangeListener { _, y, m, d ->
-            Log.d(TAG, "$y ${m + 1} $d")
-            date.text = "$y / ${m + 1} / $d"
+        // 날짜 선택
+        binding.calendar.setOnDateChangeListener { _, y, m, d ->
+            Log.d(tag, "$y ${m + 1} $d")
+            val text = "$y / ${m + 1} / $d"
+            binding.date.text = text
             year = y
             month = m + 1
             day = d
         }
     }
 
-    // 오늘 날짜 구하기
-    private fun today() {
-        val date: TextView = findViewById(R.id.date)
-        val today: String = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"))
-        year = today.slice(0..3)?.toInt()
-        month = today.slice(4..5)?.toInt()
-        day = today.slice(6..7)?.toInt()
-        date.text = "$year / $month / $day"
-        Log.d(TAG, "$today - onCreate() called")
-    }
-
-    fun complete2(v: View) {
-        Log.d(TAG, "MainActivity - onDialogClicked() called")
-
-        val saveDialog = SaveDialog(this, this)
-        saveDialog.show()
-        shows = saveDialog
+    private fun initCommit() {
+        binding.completeBtn.setOnClickListener {
+            Log.d(tag, "MainActivity - onDialogClicked() called")
+            shows.show()
+        }
     }
 
     // 패널 이벤트
     inner class PanelEventListener : SlidingUpPanelLayout.PanelSlideListener {
-        // 패널이 슬라이드 중일 때
         override fun onPanelSlide(panel: View?, slideOffset: Float) {}
 
-        // 패널의 상태가 변했을 때
         override fun onPanelStateChanged(panel: View?,
                                          previousState: SlidingUpPanelLayout.PanelState?,
                                          newState: SlidingUpPanelLayout.PanelState?) {
-            // 열림
             if (newState == SlidingUpPanelLayout.PanelState.EXPANDED) {
-                Log.d(TAG, "패널 열림")
-                var textView: TextView = findViewById(R.id.text)
+                Log.d(tag, "panel open")
+                val textView: TextView = findViewById(R.id.text)
                 try {
                     val file: FileInputStream? = openFileInput("$year$month$day.txt")
-                    val dis: DataInputStream? = DataInputStream(file)
-                    val fText: String? = dis?.readUTF()
-                    Log.d(TAG, "$fText - 패널 열림")
+                    val dis = DataInputStream(file)
+                    val fText: String? = dis.readUTF()
                     textView.text = fText
-                    dis?.close()
+                    dis.close()
+
+                    Log.d(tag, "$fText - panel open success")
                 } catch (e: java.lang.Exception) {
                     textView.text = ""
                 }
-                // 닫힘
             } else if (newState == SlidingUpPanelLayout.PanelState.COLLAPSED) {
-                Log.d(TAG, "패널 닫힘")
+                Log.d(tag, "panel close")
             }
         }
     }
 
     override fun onYes() {
-        shows!!.dismiss()
-        var textView: TextView = findViewById(R.id.text)
-        var text: String? = textView.text.toString()
-        val fos: FileOutputStream = openFileOutput("$year$month$day.txt", MODE_PRIVATE)
-        val dos: DataOutputStream = DataOutputStream(fos)
-
-        dos.writeUTF(text)
+        shows.dismiss()
+        val file: FileOutputStream = openFileOutput("$year$month$day.txt", MODE_PRIVATE)
+        val dos = DataOutputStream(file)
+        dos.writeUTF(binding.text.toString())
         dos.flush()
         dos.close()
-        Log.d(TAG, "$year$month$day 열림")
-        //textView.text = ""
+        Log.d(tag, "$year$month$day 열림")
     }
 
     override fun onNo() {
-        shows!!.dismiss()
-        Log.d(TAG, "닫힘")
+        shows.dismiss()
+        Log.d(tag, "닫힘")
     }
 }
--- app/src/main/java/com/example/diaryapp/SaveDialog.kt
+++ app/src/main/java/com/example/diaryapp/SaveDialog.kt
@@ -5,43 +5,30 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Button
+import com.example.diaryapp.databinding.DialogBinding
 
 class SaveDialog(context: Context,
-                 dialogInterface: DialogInterface)
-                : Dialog(context), View.OnClickListener {
-
-    private var dialogInterface: DialogInterface? = null
-    private val TAG: String = "로그"
-
-    init {
-        this.dialogInterface = dialogInterface
-    }
+                 private val dialogInterface: DialogInterface
+): Dialog(context), View.OnClickListener {
+    private val binding: DialogBinding by lazy { DialogBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.dialog)
+        setContentView(binding.root)
 
-        val saveBtn: Button = findViewById(R.id.dialog_btn)
-        val cancelBtn: Button = findViewById(R.id.dialog_btn1)
+        window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
-        window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
-        saveBtn.setOnClickListener(this)
-        cancelBtn.setOnClickListener(this)
+        with(binding) {
+            dialogBtn.setOnClickListener(this@SaveDialog)
+            binding.dialogBtn1.setOnClickListener(this@SaveDialog)
+        }
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.dialog_btn -> {
-                this.dialogInterface?.onYes()
-            }
-            R.id.dialog_btn1 -> {
-
-                this.dialogInterface?.onNo()
-            }
+            R.id.dialog_btn -> dialogInterface.onYes()
+            R.id.dialog_btn1 -> dialogInterface.onNo()
         }
     }
 }
